54346f3b7f07889895c66e67ebbe750622c6fb66
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-08-07 02:33:42 +0530

    Update tree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo full_aio_otfp-user
-add_lunch_combo full_aio_otfp-userdebug
-add_lunch_combo full_aio_otfp-eng
+add_lunch_combo aosp_aio_otfp-user
+add_lunch_combo aosp_aio_otfp-userdebug
+add_lunch_combo aosp_aio_otfp-eng
